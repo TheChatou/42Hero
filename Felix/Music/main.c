74e41578513a4682ac0f81b4080bd5276e97af94
chatou <chatou@Chatou.local>
2025-04-02 13:50:02 +0200

    tetris et Mario, better versions

--- Felix/Music/main.c
+++ Felix/Music/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcoullou <fcoullou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: chatou <chatou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 16:47:16 by fcoullou          #+#    #+#             */
-/*   Updated: 2025/04/01 13:48:17 by fcoullou         ###   ########.fr       */
+/*   Updated: 2025/04/02 13:48:55 by chatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,71 @@
 #define SPEAKER_PIN PB1 // Pin D9 est PB1 sur l'Arduino Uno
 
 // Fréquences des notes
-#define NOTE_C4  261
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  329
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  493
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  987
-#define NOTE_C6  1046
+#define N_00  0
+#define N_C3  131
+#define N_c3  139
+#define N_D3  147
+#define N_d3  156
+#define N_E3  165
+#define N_F3  175
+#define N_f3  185
+#define N_G3  196
+#define N_g3  208
+#define N_A3  220
+#define N_a3  233
+#define N_B3  247
+#define N_C4  261
+#define N_c4  277
+#define N_D4  294
+#define N_d4  311
+#define N_E4  329
+#define N_F4  349
+#define N_f4  370
+#define N_G4  392
+#define N_g4  415
+#define N_A4  440
+#define N_a4  466
+#define N_B4  493
+#define N_C5  523
+#define N_c5  554
+#define N_D5  587
+#define N_d5  622
+#define N_E5  659
+#define N_F5  698
+#define N_f5  740
+#define N_G5  784
+#define N_g5  831
+#define N_A5  880
+#define N_a5  932
+#define N_B5  987
+#define N_C6  1046
+
+#define T_1_8  125
+#define T_1_16 62
+#define T_1_4  250
+#define T_1_2  500
+#define T_3_4  750
+#define T_1__  1000
 
 // Fonction pour jouer une note
 void play_note(uint16_t frequency, uint16_t duration_ms)
 {
     if (frequency == 0)
     {
-        _delay_loop_2(duration_ms / 2); // Pause si la note est 0
+        PORTB &= ~(1 << SPEAKER_PIN); // S'assurer que le haut-parleur est éteint
+        _delay_loop_2(duration_ms); // Pause si la note est 0
         return;
     }
     
-    uint16_t period = (1000000 / frequency) * 5;
+    uint16_t period = (1000000 / frequency);
     uint16_t cycles = duration_ms;
 
     for (uint16_t i = 0; i < cycles; i++)
     {
         PORTB |= (1 << SPEAKER_PIN);
-        _delay_loop_2(period / 2);
+        _delay_loop_2(period);
         PORTB &= ~(1 << SPEAKER_PIN);
-        _delay_loop_2(period / 2);
+        _delay_loop_2(period);
     }
 }
 
@@ -67,83 +88,71 @@ int main(void)
     // Configure PB1 comme une sortie
     DDRB |= (1 << SPEAKER_PIN);
     
-    // uint16_t notes[] = { 
-    //     NOTE_E5, NOTE_E5, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_D5, NOTE_C5, NOTE_B4, 
-    //     NOTE_A4, NOTE_A4, NOTE_A4, NOTE_C5, NOTE_E5, NOTE_A4, NOTE_A4,
-    //     NOTE_D5, NOTE_F5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_C5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_B4, NOTE_B4
-    // };
-
-    // uint16_t notes[] = { 
-    //     NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, 
-    //     NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, 
-    //     NOTE_C6
-    // };
-    
-    // uint16_t notes[] = { 
-    //     NOTE_E5, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_B4, NOTE_A4, NOTE_A4, 
-    //     NOTE_C5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_B4, NOTE_A4, NOTE_A4,
-    //     NOTE_D5, NOTE_F5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_C5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_B4, NOTE_B4
-    // };
-
-    // uint16_t durations[] = { 
-    //     2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 
-    //     2000, 2000, 2000, 2000, 2000, 2000, 2000 
-    // };
-
-// // Super Mario
+// // Super Mario Bros Theme - Ajusté avec un tempo plus précis
 //     uint16_t notes[] = {
-//         NOTE_E5, NOTE_E5, 0, NOTE_E5, 0, NOTE_C5, NOTE_E5, 0,
-//         NOTE_G5, 0, 0, 0, NOTE_G4, 0, 0, 0,
-//         NOTE_C5, 0, 0, NOTE_G4, 0, 0, NOTE_E4, 0,
-//         0, NOTE_A4, 0, NOTE_B4, 0, NOTE_B4, NOTE_A4, 0,
-//         NOTE_G4, NOTE_E5, NOTE_G5, NOTE_A5, 0, NOTE_F5, NOTE_G5, 0,
-//         NOTE_E5, NOTE_C5, NOTE_D5, NOTE_B4, 0, 0, NOTE_C5, 0,
-//         0, NOTE_G4, 0, NOTE_E4, 0, NOTE_A4, 0, NOTE_B4,
-//         0, NOTE_A4, NOTE_G4, 0, NOTE_E5, 0, NOTE_G5, NOTE_A5,
-//         0, NOTE_F5, NOTE_G5, 0, NOTE_E5, NOTE_C5, NOTE_D5, NOTE_B4
+//         N_E5, N_E5, N_00, N_E5, N_00, N_C5, N_E5, N_00,
+//         N_G5, N_00, N_00, N_00, N_G4, N_00, N_00, N_00,
+//         N_C5, N_00, N_00, N_G4, N_00, N_00, N_E4, N_00,
+//         N_00, N_A4, N_00, N_B4, N_00, N_B4, N_A4, N_00,
+//         N_G4, N_E5, N_G5, N_A5, N_00, N_F5, N_G5, N_00,
+//         N_E5, N_C5, N_D5, N_B4, N_00, N_00, N_C5, N_00,
+//         N_00, N_G4, N_00, N_E4, N_00, N_A4, N_00, N_B4,
+//         N_00, N_A4, N_G4, N_00, N_E5, N_00, N_G5, N_A5,
+//         N_00, N_F5, N_G5, N_00, N_E5, N_C5, N_D5, N_B4,
+//         // Suite
+//         N_00, N_G5, N_F5, N_E5, N_D5, N_C5, N_00, N_E4, N_G4,
+//         N_A4, N_F4, N_G4, N_00, N_E4, N_C4, N_D4, N_B3, N_G3,
+//         N_00, N_E5, N_G5, N_A5, N_F5, N_G5, N_00, N_E5, N_C5, N_D5,
+//         N_B4, N_G4, N_00, N_E4, N_C4, N_D4, N_B3, N_A3, N_G3,
+//         N_G4, N_A4, N_G4, N_00, N_E4, N_G4, N_A4, N_F4, N_G4,
+//         N_00, N_E4, N_C4, N_D4, N_B3, N_G3, N_E3, N_C3, N_A3
 //     };
-
+    
 //     uint16_t durations[] = {
-//         150, 150, 150, 150, 150, 150, 150, 150,
-//         150, 300, 150, 300, 150, 300, 150, 300,
-//         150, 300, 150, 150, 150, 150, 150, 150,
-//         150, 150, 150, 150, 150, 150, 150, 150,
-//         150, 150, 150, 150, 150, 150, 150, 150,
-//         150, 150, 150, 150, 150, 300, 150, 300,
-//         150, 300, 150, 150, 150, 150, 150, 150,
-//         150, 150, 150, 150, 150, 150, 150, 150,
-//         150, 150, 150, 150, 150, 150, 150, 150
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_4, T_1_8, T_1_4, T_1_8, T_1_4, T_1_8, T_1_4,
+//         T_1_8, T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_4, T_1_8, T_1_4,
+//         T_1_8, T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         // Suite
+//         T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_4, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8,
+//         T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8, T_1_8
 //     };
 
-// Tetris
+    // Tetris Theme - Réarrangé pour refléter la mélodie originale
     uint16_t notes[] = {
-        NOTE_E4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_C5, NOTE_B4,
-        NOTE_A4, NOTE_A4, NOTE_C5, NOTE_E5, NOTE_D5, NOTE_C5,
-        NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_A4,
-        NOTE_A4, NOTE_C5, NOTE_E5, NOTE_A5, NOTE_A5, NOTE_A5,
-        NOTE_G5, NOTE_F5, NOTE_E5, NOTE_C5, NOTE_E5, NOTE_D5,
-        NOTE_C5, NOTE_B4, NOTE_B4, NOTE_C5, NOTE_D5, NOTE_E5,
-        NOTE_C5, NOTE_A4, NOTE_A4, NOTE_A4
+        N_E5, N_B4, N_C5, N_D5, N_C5, N_B4, N_A4, N_A4,
+        N_C5, N_E5, N_D5, N_C5, N_B4, N_C5, N_D5, N_E5,
+        N_C5, N_A4, N_A4, N_A4, N_B4, N_C5, N_D5, N_F5,
+        N_A5, N_G5, N_F5, N_E5, N_C5, N_E5, N_D5, N_C5,
+        N_B4, N_B4, N_C5, N_D5, N_E5, N_C5, N_A4, N_A4
     };
 
     uint16_t durations[] = {
-        300, 150, 300, 150, 300, 300,
-        300, 150, 300, 300, 150, 300,
-        300, 150, 300, 300, 300, 300,
-        300, 150, 300, 300, 150, 300,
-        300, 150, 300, 300, 150, 300,
-        300, 150, 300, 300, 150, 300,
-        300, 150, 300, 300
+        T_1_4, T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_4, T_1_8,
+        T_1_8, T_1_4, T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_4,
+        T_1_4, T_1_4, T_1_8, T_1_8, T_1_8, T_1_8, T_1_4, T_1_8,
+        T_1_8, T_1_4, T_1_8, T_1_8, T_1_4, T_1_8, T_1_8, T_1_4,
+        T_1_4, T_1_8, T_1_8, T_1_4, T_1_4, T_1_4, T_1_4, T_1_4
     };
+
     
     while (1)
     {
         for (size_t i = 0; i < sizeof(notes) / sizeof(notes[0]); i++)
         {
-            play_note(notes[i], durations[i] / 2);
-            _delay_ms(50);
+            play_note(notes[i], durations[i]);
+            _delay_ms(100);
         }
-        _delay_ms(200);
+        _delay_ms(2000);
     }
 
     return 0;
